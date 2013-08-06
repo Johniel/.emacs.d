543a3bfe5d7d282664857962ee188a80f7052149
___Johniel <johniel@Haze.(none)>
2013-08-07 00:11:54 +0900

    add twig snipets

--- conf/setup-web-mode.el
+++ conf/setup-web-mode.el
@@ -14,6 +14,10 @@
              (setq ac-sources '(ac-source-words-in-buffer
                                 ac-source-words-in-same-mode-buffers))))
 
+(setq web-mode-markup-indent-offset 2) ;; html indent
+(setq web-mode-css-indent-offset 2)    ;; css indent
+(setq web-mode-code-indent-offset 2)   ;; script indent(js,php,etc..)
+
 (add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
 
 (provide 'setup-web-mode)
--- mode-mappings.el
+++ mode-mappings.el
@@ -1,1 +1,2 @@
 (add-to-list 'auto-mode-alist '("\\.zsh\\'"  . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.snip\\'"  . snippet-mode))
