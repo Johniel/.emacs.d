d95cb4f6d80e8ba3129d3a2e1a4008ae61e0dbd1
___Johniel <johniel@Haze.(none)>
2013-09-11 02:23:49 +0900

    add web-mode face setting

--- theme/piecewise-linear-theme.el
+++ theme/piecewise-linear-theme.el
@@ -77,6 +77,19 @@
  '(highlight-indentation-face ((t (:background "#1a1a1a"))))
  '(highlight-indentation-current-column-face ((t (:background "#2a2040"))))
 
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ ;; web-mode
+ ;;
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ '(web-mode-doctype-face          ((t (:foreground "#82AE46"))))
+ '(web-mode-html-tag-face         ((t (:foreground "#7a7a7a" :weight bold))))
+ '(web-mode-html-attr-name-face   ((t (:foreground "#C97586"))))
+ '(web-mode-html-attr-value-face  ((t (:foreground "#82AE46"))))
+ '(web-mode-comment-face          ((t (:foreground "#D9333F"))))
+ '(web-mode-server-comment-face   ((t (:foreground "#D9333F"))))
+ '(web-mode-css-rule-face         ((t (:foreground "#A0D8EF"))))
+ '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-at-rule-face      ((t (:foreground "#FF7F00"))))
  )
 
 ;;;###autoload
