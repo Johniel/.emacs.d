14abe57aa6d6aff756c4f81aee4c0f291367e61c
Johniel <johniel.s.m@gmail.com>
2013-10-28 06:50:49 +0900

    indent style

--- conf/setup-php-mode.el
+++ conf/setup-php-mode.el
@@ -3,4 +3,9 @@
 
 (add-hook 'php-mode-hook 'php-eldoc-enable)
 
+(add-hook 'php-mode-hook (lambda ()
+                           (setq indent-tabs-mode nil)
+                           (setq tab-width 4)
+                           (setq c-basic-offset 4)))
+
 (provide 'setup-php-mode)
--- conf/setup-web-mode.el
+++ conf/setup-web-mode.el
@@ -14,9 +14,9 @@
              (setq ac-sources '(ac-source-words-in-buffer
                                 ac-source-words-in-same-mode-buffers))))
 
-(setq web-mode-markup-indent-offset 2) ;; html indent
-(setq web-mode-css-indent-offset 2)    ;; css indent
-(setq web-mode-code-indent-offset 2)   ;; script indent(js,php,etc..)
+(setq web-mode-markup-indent-offset 4) ;; html indent
+(setq web-mode-css-indent-offset  4)    ;; css indent
+(setq web-mode-code-indent-offset 4)   ;; script indent(js,php,etc..)
 
 (add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
 
