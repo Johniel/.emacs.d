95821b176ff76a6fc6e6caf4a1ceda561200b9ef
Johniel <johniel.s.m@gmail.com>
2014-10-12 21:06:55 +0900

    show more candidates

--- conf/setup-popwin.el
+++ conf/setup-popwin.el
@@ -1,7 +1,7 @@
 (require 'popwin)
 
 (setq display-buffer-function 'popwin:display-buffer)
-(setq popwin:popup-window-height 18)
+(setq popwin:popup-window-height 25)
 
 (push "*Backtrace*" popwin:special-display-config)
 (push "*helm mini*" popwin:special-display-config)
