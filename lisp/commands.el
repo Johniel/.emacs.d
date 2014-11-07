6acdb48fc60eb407385851cd24838d2e7c729ff4
johniel.s.m@gmail.com <johniel.s.m@gmail.com>
2014-11-07 17:00:58 +0900

    interactive functions are defined in commands.el

--- lisp/commands.el
+++ lisp/commands.el
@@ -185,5 +185,31 @@
                            snake-txt
                          camel-txt))))))
 
+;;
+;; http://www.bookshelf.jp/soft/meadow_15.html#SEC119
+(defun describe-face-at-point ()
+  "Return face used at point."
+  (interactive)
+  (message "%s" (get-char-property (point) 'face)))
+
+;; http://tomykaira.hatenablog.com/entry/2013/01/25/000057
+;;
+(defun increment-decimal-number (&optional arg)
+  "Increment the number forward from point by 'arg'."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "0123456789")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                 answer)))))))
+
+
 ;;;
 (provide 'commands)
