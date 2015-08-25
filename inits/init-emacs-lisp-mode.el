e3e6bd2e1825b9f50bb1dc811d36ec41961bf08b
johniel.s.m@gmail.com <johniel.s.m@gmail.com>
2015-08-25 20:24:01 +0900

    update

--- inits/init-emacs-lisp-mode.el
+++ inits/init-emacs-lisp-mode.el
@@ -3,9 +3,6 @@
 (def-face elisp-special "khaki" "elisp special")
 (def-face elisp-keyword "cyan1" "additional elisp keyword")
 
-;; (add-keywords 'emacs-lisp-mode '("nil") 'elisp-special)
-;; (add-keywords 'emacs-lisp-mode '("require-package") 'elisp-keyword)
-
 (define-key emacs-lisp-mode-map (kbd "C-x e") 'pp-macroexpand-last-sexp)
 
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
@@ -13,10 +10,5 @@
 (require 'elisp-slime-nav)
 (autoload 'elisp-slime-nav-mode "elisp-slime-nav")
 (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
-;; (eval-after-load 'elisp-slime-nav '(diminish 'elisp-slime-nav-mode))
-
-(add-hook 'emacs-lisp-mode-hook (lambda ()
-                                  (my-ac-emacs-lisp-mode-setup)))
-
 
 (provide 'init-emacs-lisp-mode)
