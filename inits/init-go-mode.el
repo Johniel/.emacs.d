2eb14bfadbe6e9848efed370268bcc16b81a2882
___Johniel <johniel.s.m@gmail.com>
2017-10-16 13:54:42 +0900

    Update init-go-mode.el

--- inits/init-go-mode.el
+++ inits/init-go-mode.el
@@ -4,25 +4,28 @@
 (require 'exec-path-from-shell)
 (require 'go-eldoc)
 
-;; https://github.com/nsf/gocode/tree/master/emacs-company
-(setq company-tooltip-limit 20)                      ; bigger popup window
-(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
-(setq company-echo-delay 0)                          ; remove annoying blinking
-(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+(require 's)
+
+(defvar gopath (exec-path-from-shell-getenv "GOPATH"))
+
+(setq company-tooltip-limit 20)
+(setq company-idle-delay 0)
+(setq company-echo-delay 0)
+(setq company-begin-commands '(self-insert-command))
 
 (defun init-go-mode-hook()
   (add-hook 'before-save-hook' 'gofmt-before-save)
   (set (make-local-variable 'company-backends) '(company-go))
   (company-mode)
   (go-eldoc-setup)
+  (flycheck-mode)
   (setq indent-tabs-mode nil)
   (setq c-basic-offset 4)
   (setq tab-width 4)
   t)
 
 (add-hook 'go-mode-hook 'init-go-mode-hook)
-
-
-(defconst gopath (exec-path-from-shell-getenv "GOPATH"))
+(add-hook 'before-save-hook 'gofmt-before-save)
+(define-key go-mode-map (kbd "M-.") 'godef-jump-other-window)
 
 (provide 'init-go-mode)
