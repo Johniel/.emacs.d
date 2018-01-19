4b473ae2e219c6fc1829f85a40ab2cb3047ac125
___Johniel <johniel.s.m@gmail.com>
2018-01-19 18:21:00 +0900

    Update init-php-mode.el

--- inits/init-php-mode.el
+++ inits/init-php-mode.el
@@ -6,11 +6,27 @@
       ad-do-it
     nil))
 
-(add-hook 'php-mode-hook 'php-eldoc-enable)
+(defun johniel::php-mode-hook()
+  ;; style
+  (setq indent-tabs-mode nil)
+  (setq tab-width 4)
+  (setq c-basic-offset 4)
+  ;; php-eldoc
+  (php-eldoc-enable)
+  ;; ac-php
+  (require 'company-php)
+  (company-mode t)
+  (make-local-variable 'company-backends)
+  (add-to-list 'company-backends 'company-ac-php-backend)
+  (define-key php-mode-map (kbd "M-.") 'ac-php-find-symbol-at-point)
+  (define-key php-mode-map (kbd "M-,") 'ac-php-location-stack-back)
+  (setq company-tooltip-limit 20)
+  (setq company-idle-delay 0)
+  (setq company-echo-delay 0)
+  (setq company-begin-commands '(self-insert-command))
+  (ac-php-core-eldoc-setup)
+  true)
 
-(add-hook 'php-mode-hook (lambda ()
-                           (setq indent-tabs-mode nil)
-                           (setq tab-width 4)
-                           (setq c-basic-offset 4)))
+(add-hook 'php-mode-hook 'johniel::php-mode-hook)
 
 (provide 'init-php-mode)
