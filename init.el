1f4677b8185dd4c0879640ea6d6a98168f9e4999
___Johniel <johniel.s.m@gmail.com>
2018-04-24 12:19:07 +0900

    Update init.el

--- init.el
+++ init.el
@@ -1,13 +1,5 @@
 ;;; init.el --- ___Johniel's init file
 
-;;
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (unless (version<= "25" emacs-version)
   (error "Use Emacs 25 or higher"))
 
@@ -17,6 +9,7 @@
 (require 'load-package)
 
 ;;
+(package-initialize)
 (install-packages '(ac-php
                     ace-jump-helm-line
                     ace-jump-mode
