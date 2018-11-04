49dcbdc60284b26518ea8456c6f0ac361803b9ec
Johniel <motohazu@mercari.com>
2018-11-04 19:10:04 +0900

    setup rust

---
+++ inits/init-rust-mode.el
@@ -0,0 +1,16 @@
+(require 'rust-mode)
+(require 'racer)
+
+(if (commandp "rust-format")
+    (eval-after-load "rust-mode"
+      '(setq-default rust-format-on-save t))
+  (message "command not found: rust-format"))
+
+(add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+
+(setq company-tooltip-align-annotations t)
+
+(provide 'init-rust-mde)
