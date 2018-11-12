cf7a837eccb7ed3595ba0f32bcf0f4fdf2abdb63
Johniel <motohazu@mercari.com>
2018-11-12 15:23:54 +0900

    update

--- inits/init-rust-mode.el
+++ inits/init-rust-mode.el
@@ -8,11 +8,13 @@
 
 (add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
 
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'rust-mode-hook (lambda ()
+                            (racer-mode)
+                            (eldoc-mode)
+                            (add-hook 'before-save-hook 'rust-format-buffer)))
 
 (setq company-tooltip-align-annotations t)
 
-(add-hook 'before-save-hook 'rust-format-buffer)
+
 
 (provide 'init-rust-mde)
