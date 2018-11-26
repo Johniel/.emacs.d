ae86922703966b49441f3706ca0feae288a58b4e
Johniel <motohazu@mercari.com>
2018-11-26 18:14:33 +0900

    update

--- inits/init-c++-mode.el
+++ inits/init-c++-mode.el
@@ -5,8 +5,8 @@
   (when (eq major-mode 'c++-mode)
     (clang-format-buffer "file")))
 
-(if (executable-find "clang-format")
-    (add-hook 'before-save-hook 'clang-format-format-buffer))
+;; (if (executable-find "clang-format")
+;;     (add-hook 'before-save-hook 'clang-format-format-buffer))
 
 (def-face c++-macro "cyan1" "C++ macro form")
 (add-keywords 'c++-mode '("each" "unless") 'c++-macro)
@@ -20,7 +20,6 @@
              (c-toggle-electric-state +1)
              (setq c-basic-offset 2)
              (setq tab-width 2)
-             (c-set-offset 'inlambda 0)
-             (block-open . 0)))
+             (c-set-offset 'inlambda 0)))
 
 (provide 'init-c++-mode)
