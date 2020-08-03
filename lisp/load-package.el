cd70f3453b62740dd6f38a5fb50155b55fdee2bc
___Johniel <johniel.s.m@gmail.com>
2020-08-03 17:42:18 +0900

    Update load-package.el

--- lisp/load-package.el
+++ lisp/load-package.el
@@ -2,7 +2,7 @@
 
 ;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 ;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;;
