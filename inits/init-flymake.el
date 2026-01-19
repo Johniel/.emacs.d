902d83a0ad0d68efb76d6397d182b908284f6eba
Johniel <johniel.s.m@gmail.com>
2026-01-19 19:56:32 +0900

    Revert "flymake modernization and LSP C++ integration"
    This reverts commit d80af14b0d85b7686b8fcb671be903788c8ca9cb.

---
+++ inits/init-flymake.el
@@ -0,0 +1,98 @@
+;;; init-flymake.el ---
+
+(require 'flymake)
+(require 'flymake-proc)
+
+;;
+(setq flymake-start-syntax-check-on-newline nil)
+
+;; -1 = NONE,
+;;  0 = ERROR,
+;;  1 = WARNING,
+;;  2 = INFO,
+;;  3 = DEBUG
+(setq flymake-log-level -1)
+
+;; display flymake error in minibuffer
+;; https://gist.github.com/415429
+(defun my-flymake-show-help ()
+  (when (get-char-property (point) 'flymake-overlay)
+    (let ((help (get-char-property (point) 'help-echo)))
+      (if help (message "%s" help)))))
+
+(add-hook 'post-command-hook 'my-flymake-show-help)
+
+;;
+;; C++
+(defun flymake-cpp-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "g++" (list "-Wall" "-fsyntax-only" "-std=c++20" local-file))))
+
+(push '("\\.cpp$" flymake-cpp-init) flymake-proc-allowed-file-name-masks)
+(push '("\\.hpp$" flymake-cpp-init) flymake-proc-allowed-file-name-masks)
+
+(add-hook 'c++-mode-hook (lambda () (flymake-mode t)))
+
+;;
+;; C
+(defun flymake-c-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "gcc" (list "-Wall" "-fsyntax-only" local-file))))
+
+(push '("\\.c$" flymake-c-init) flymake-proc-allowed-file-name-masks)
+(push '("\\.h$" flymake-c-init) flymake-proc-allowed-file-name-masks)
+
+(add-hook 'c-mode-hook (lambda () (flymake-mode t)))
+
+;; Emacs Lisp
+;; http://www.lunaport.net/blog/2010/02/windowsflymake-elisp-1.html
+(defun flymake-elisp-init ()
+  (unless (string-match "^ " (buffer-name))
+    (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+           (local-file  (file-relative-name
+                         temp-file
+                         (file-name-directory buffer-file-name))))
+      (list
+       (expand-file-name invocation-name invocation-directory)
+       (list
+        "-Q" "--batch" "--eval"
+        (prin1-to-string
+         (quote
+          (dolist (file command-line-args-left)
+            (with-temp-buffer
+              (insert-file-contents file)
+              (emacs-lisp-mode)
+              (let ((parse-sexp-ignore-comments t))
+                (condition-case data
+                    (scan-sexps (point-min) (point-max))
+                  (scan-error
+                   (goto-char(nth 2 data))
+                   (princ (format "%s:%s: error: Unmatched bracket or quote\n"
+                                  file (line-number-at-pos))))))))))
+        local-file)))))
+(push '("\\.el$" flymake-elisp-init) flymake-proc-allowed-file-name-masks)
+(add-hook 'emacs-lisp-mode-hook
+          ;; workaround for (eq buffer-file-name nil)
+          (function (lambda () (if buffer-file-name (flymake-mode)))))
+
+;; buffer has a running process: kill it?
+;; http://stackoverflow.com/questions/7299893/getting-rid-of-buffer-has-running-process-confirmation-when-the-process-is-a-f
+(defun my-flymake-set-no-query-on-exit (proc)
+  "Set flag to allow exit without query on flymake processes."
+  (when proc
+    (set-process-query-on-exit-flag proc nil))
+  proc)
+
+(advice-add 'flymake-start-syntax-check-process :filter-return #'my-flymake-set-no-query-on-exit)
+
+
+(provide 'init-flymake)
